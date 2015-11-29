06873f37c584d71f9ba984e7c220b437e1d83a8a
Youssef Salim <salimyoussef11@yahoo.fr>
2015-11-29 23:29:31 +0100

    Topologie avec KafkaSpout

--- src/main/java/TopologyMain.java
+++ src/main/java/TopologyMain.java
@@ -1,3 +1,5 @@
+import backtype.storm.spout.SchemeAsMultiScheme;
+import kafka.api.OffsetRequest;
 import spouts.InputReader;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
@@ -5,36 +7,46 @@ import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import bolts.InputCompareToDB;
 import bolts.InputNormalizer;
-
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
 
 
 public class TopologyMain {
-	public static void main(String[] args) throws InterruptedException {
+        public static void main(String[] args) throws InterruptedException {
+                TopologyBuilder builder=new TopologyBuilder();
 
-		TopologyBuilder builder = new TopologyBuilder();
+                ZkHosts zkHosts = new ZkHosts("localhost:2181");
+                String topic = "coord";
+                String consumer_group_id = "id7";
+                SpoutConfig kafkaConfig = new SpoutConfig(zkHosts, topic, "", consumer_group_id);
 
-		builder.setSpout("input-reader",new InputReader());
+                kafkaConfig.startOffsetTime = OffsetRequest.EarliestTime();
+                kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
 
-		builder.setBolt("input-normalizer", new InputNormalizer()).shuffleGrouping("input-reader");
-		builder.setBolt("input-compareToDB", new InputCompareToDB(),1).fieldsGrouping("input-normalizer", new Fields("inputcoord"));
+                KafkaSpout kafkaSpout = new KafkaSpout(kafkaConfig);
+                builder.setSpout("KafkaSpout", kafkaSpout);
+                builder.setBolt("input-normalizer", new InputNormalizer()).shuffleGrouping("KafkaSpout");
+                builder.setBolt("input-compareToDB", new InputCompareToDB(), 1).fieldsGrouping("input-normalizer", new Fields("inputcoord"));
 
-		Config conf;
-		conf = new Config();
-		conf.put("inputFile", args[0]);
-		conf.setDebug(false);
+                Config conf;
+                conf = new Config();
+                //conf.put("inputFile", args[0]);
+                conf.setDebug(false);
 
-		conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
-		LocalCluster cluster = new LocalCluster();
+                conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+                LocalCluster cluster = new LocalCluster();
 
-		System.err.println("START!!!!!!!!!!!!!!!!!!!!");
+                System.err.println("START!!!!!!!!!!!!!!!!!!!!");
 
-		cluster.submitTopology("Getting-Started-Toplogie", conf, builder.createTopology());
+                cluster.submitTopology("Getting-Started-Topology", conf, builder.createTopology());
 
-		System.err.println("Thread.sleep(4000)!!!!!!!!!!!!!!!!!!!!");
+                System.err.println("Thread.sleep(4000)!!!!!!!!!!!!!!!!!!!!");
 
-		Thread.sleep(4000);
-		cluster.shutdown();
+                Thread.sleep(4000);
+                cluster.shutdown();
 
-		System.err.println("END!!!!!!!!!!!!!!!!!!!!");
+                System.err.println("END!!!!!!!!!!!!!!!!!!!!");
 	}
 }
